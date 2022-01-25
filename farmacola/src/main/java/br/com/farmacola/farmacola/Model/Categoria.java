0916d32b71bceb2bc998a4383c1e4cf689a599b7
Rodrigo667k <95310379+Rodrigo667k@users.noreply.github.com>
2022-01-25 18:50:05 -0300

    Update Categoria.java

--- farmacola/src/main/java/br/com/farmacola/farmacola/Model/Categoria.java
+++ farmacola/src/main/java/br/com/farmacola/farmacola/Model/Categoria.java
@@ -16,27 +16,30 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity
-@Table(name ="tb_categoria")
+@Entity //entidade 
+@Table(name ="tb_categoria") //nome da tabela no banco de dados
 public class Categoria {
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id//identificador 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//para falar que o campo mapeado será gerado automaticamente pelo banco de dados
 	private long id; 
 	
 	@NotNull // Não permite atributos com valor nulo
-	@Size(min = 20, max = 255,message = "Valor mínimo de 20 caracteres e máximo de 255")
+	@Size(min = 20, max = 255,message = "Valor mínimo de 20 caracteres e máximo de 255")/* serve para falar que o campo mapeado será gerado automaticamente pelo banco de dados,
+	impede que o usuario coleque caracteres menor que 5 e maior que 255*/
 	private String descricao;
 	
 	@NotNull // Não permite atributos com valor nulo
-	@Size(min = 5, max = 255,message = "Valor mínimo de 5 caracteres e máximo de 255") 
+	@Size(min = 5, max = 255,message = "Valor mínimo de 5 caracteres e máximo de 255")/* serve para falar que o campo mapeado 
+	será gerado automaticamente pelo banco de dados, impede que o usuario coleque caracteres menor que 5 e maior que 255 */
 	private String categoria;
 	
-	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
-	@JsonIgnoreProperties("categoria")
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)/*permite excluir ou atualizar os registros relacionados presentes na tabela filha 
+	automaticamente */
+	@JsonIgnoreProperties("categoria")//permite um controle mais preciso sobre quais objetos devem ignorar campos desconhecidos e quais não devem.
 	private List <Produto> produto;
 	
-
+	//Get set
 	public long getId() {
 		return id;
 	}
